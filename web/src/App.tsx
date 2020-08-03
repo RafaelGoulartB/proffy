5dfbb46d24fc8f620a8cda07c2145f932db872e3
RafaelGoulartB <rafagoulartb@gmail.com>
2020-08-03 10:57:25 -0300

    📦 feat: responsive initial page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 import "./assets/styles/global.css";
+import Landing from "./pages/Landing";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./styles.css";
+
+import logoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+export default function Landing() {
+  return (
+    <div id="page-landing">
+      <div className="container" id="page-landing-content">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua Plataforma de estudos online</h2>
+        </div>
+
+        <img
+          src={landingImg}
+          alt="Plataforma de Estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="#" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
+          <a href="#" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar Aulas" />
+            Dar Aulas
+          </a>
+        </div>
+
+        <span className="total-connections">
+          Total de 200 conexões realizadas.{" "}
+          <img src={purpleHeartIcon} alt="Coração Roxo" />
+        </span>
+      </div>
+    </div>
+  );
+}
