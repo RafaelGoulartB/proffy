0b287f024c2ffc1832515e6e2deb8f66d69ea1b6
RafaelGoulartB <rafagoulartb@gmail.com>
2020-08-05 09:05:32 -0300

    📦 feat: add description to header

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,6 +8,7 @@ import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -22,6 +23,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
         {props.children}
       </div>
     </header>
