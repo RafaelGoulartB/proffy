29a4705c890f7543ae69e79a5806986a26b0b742
RafaelGoulartB <rafagoulartb@gmail.com>
2020-08-05 08:54:25 -0300

    📦 feat: add input compoment

---
+++ web/src/components/Input/index.tsx
@@ -0,0 +1,19 @@
+import React, { InputHTMLAttributes } from "react";
+
+import "./styles.css";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, label, ...rest }) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>{label}</label>
+      <input type="text" id={name} {...rest} />
+    </div>
+  );
+};
+
+export default Input;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,24 +3,18 @@ import "./styles.css";
 
 import PageHeader from "../../components/PageHeader/";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
 
 export default function TeacherList() {
   return (
     <div className="container" id="page-teacher-list">
       <PageHeader title="Esses são os Proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input name="subject" label="Matéria" />
+
+          <Input name="week_day" label="Dia da Semana" />
+
+          <Input name="time" label="Hora" type="time" />
         </form>
       </PageHeader>
 
